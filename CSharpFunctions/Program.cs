9e0b4c7fc33afb15278d434391835dd599f3534b
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-05 13:35:16 +0200

    Add project files.

---
+++ CSharpFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*************************funzioni*********************************/
+//stampa array
+void stampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+
+
+
+
+/****************PROGRAMMA PRINCIPALE*******************/
+int[] arrayDiNumeri = { 2, 5, 6, 8, 9, 1 };
+stampaArray(arrayDiNumeri);
